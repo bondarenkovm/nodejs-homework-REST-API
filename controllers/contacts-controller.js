b6c184804f818b8249a6251a26db8fdd1e43fcd0
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-05-29 18:24:44 +0300

    Added contact filtering in contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,9 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+
+  const query = favorite ? { owner, favorite } : { owner };
+
+  const result = await Contact.find(query, "-createdAt -updatedAt", {
     skip,
     limit,
   });
