40fb724beec02e53c2903281f6eb5830ec614864
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-06-04 12:10:32 +0300

    Added processing of the avatar

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
@@ -80,11 +81,17 @@ const updateSubscription = async (req, res) => {
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
-  const { path: temtUpload, originalname } = req.file;
+  const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(temtUpload, resultUpload);
+  await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
+
+  Jimp.read(resultUpload, (error, image) => {
+    if (error) throw HttpError(404, "Avatar not found");
+    image.resize(250, 250).write(resultUpload);
+  });
+
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
     avatarURL,
